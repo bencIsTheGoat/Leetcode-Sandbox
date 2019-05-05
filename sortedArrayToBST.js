a31608114e54a8855c1f7541d5c8a25702afb5ad
Ben Cutler <bcutler94@gmail.com>
2019-05-05 09:54:03 -0400

    Comment sorttobst

--- sortedArrayToBST.js
+++ sortedArrayToBST.js
@@ -1,13 +1,18 @@
-// grab middle idx and create node
-// set left and right to recur call of left and right slice
+// Convert sorted array to binary search tree
+
 var sortedArrayToBST = function (nums) {
+    // if the length of current array is empty return null since no left/right node
     if (nums.length === 0) return null;
+    // grab the middle index of the array in order to find parent node
     const midIdx = Math.floor(nums.length / 2);
+    // create node instance with element in nums
     const node = new TreeNode(nums[midIdx]);
+    // get left subtree
     const left = nums.slice(0, midIdx);
+    // get right subtree
     const right = nums.slice(midIdx + 1);
+    // initialize left and right node children with subtrees by recursive call
     node.right = sortedArrayToBST(right);
     node.left = sortedArrayToBST(left);
-
     return node;
 };
