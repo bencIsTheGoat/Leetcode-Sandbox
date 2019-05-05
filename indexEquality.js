26ef0821b97c1f43274625a4723e2557cc887ca8
Ben Cutler <bcutler94@gmail.com>
2019-05-05 14:17:43 -0400

    Add comments

--- indexEquality.js
+++ indexEquality.js
@@ -4,18 +4,28 @@
 // is equal to the value at that index, otherwise return -1
 
 function indexEquality(array) {
+    // grab the middle index of the sorted array, indicates the 'root' node
+    // of binary tree
     let middleIdx = Math.floor(array.length / 2);
     let current = array[middleIdx];
-    let log = Math.ceil(Math.log2(array.length + 1)) - 1;;
+    // find the height of tree for max number of iterations
+    let log = Math.ceil(Math.log2(array.length + 1)) - 1;
+    // keep track of current height of tree for the middle index of the right
+    // side of the sorted array, want to make sure we are finding the middle index of
+    // increasingly shorter range
     let rightCount = 0
     while (log >= 0) {
         rightCount += 1;
         log -= 1
+        // if the index and element equal return the element
         if (array[middleIdx] === middleIdx) {
             return array[middleIdx];
         } else if (middleIdx < current) {
+            // if the index is less than the middle element we know we have to 
+            // go down the left side of the tree
             middleIdx = Math.floor(middleIdx / 2);
         } else {
+            // otherwise we go down the right side of the tree
             middleIdx = middleIdx + Math.round(middleIdx / (2 * rightCount));
         } 
     }
