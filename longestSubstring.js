7a6244daf7b694f503e23f3016fc2236c106d54f
Ben Cutler <bcutler94@gmail.com>
2019-05-05 09:48:51 -0400

    Comment longestSubstring

--- longestSubstring.js
+++ longestSubstring.js
@@ -3,20 +3,33 @@
 // keep running and total count of length of string
 // if running > total then replace total
 // reset running if string includes letter
-var lengthOfLongestSubstring = function (s) {
 
+// Find the length of longest substring (unique letters)
+
+var lengthOfLongestSubstring = function (s) {
+    // initialize substring and running/total counts
     let subString = '';
     let runningCount = 0;
     let totalCount = 0;
+    // iterate thru string
     for (let i = 0; i < s.length; i++) {
+        // if the substring doesnt include the current letter then add it to
+        // substring
         if (!subString.includes(s[i])) {
             subString += s[i];
+            // set the running count to the length of substring in order to compare
+            // to total count
             runningCount = subString.length;
         } else {
+            // if the substring does include the current letter, then grab the index
+            // of letter one of ahead of the same letter in substring to reset
+            // substring
             let badIdx = subString.indexOf(s[i]);
+            // reset substring
             subString = subString.slice(badIdx + 1) + s[i];
             runningCount = 0;
         }
+        // if the running count is greater than total count then update total count
         if (runningCount > totalCount) totalCount = runningCount;
     }
     return totalCount;
