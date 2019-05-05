4116d54e4cedc1a56a292cb33122c1cca1032b7a
Ben Cutler <bcutler94@gmail.com>
2019-05-05 09:11:53 -0400

    Comment isbalanced

--- isBalanced.js
+++ isBalanced.js
@@ -1,5 +1,9 @@
 function getHeight(root) {
+    // return -1 when the node is empty, since empty nodes have height of -1
     if (!root) return -1;
+    // recursively calls the function of the nodes right and left children,
+    // taking the maximum of the height of either the left or right tree
+    // add 1 to indicate a recursive call has been made, aka step down a level of tree
     return 1 + Math.max(getHeight(root.right), getHeight(root.left));
 }
 
