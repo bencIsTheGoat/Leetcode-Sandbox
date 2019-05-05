1fda8dbefaf57977362a804d32f3fb89f8c9cfb3
Ben Cutler <bcutler94@gmail.com>
2019-05-05 08:58:23 -0400

    comment canfinish

--- canFinish.js
+++ canFinish.js
@@ -1,4 +1,5 @@
 var canFinish = function (numCourses, prerequisites) {
+    // convert edge list to adjacency list
     let adjList = convertToAdjList(prerequisites);
     numCourses = Object.keys(adjList).length;
     let set = new Set();
@@ -18,14 +19,20 @@ var canFinish = function (numCourses, prerequisites) {
 };
 
 function convertToAdjList(edgeList) {
+    // convert edge to adjacency list
     let adj = {};
+    // edge list is array of pairs
     edgeList.forEach(node => {
+        // first convert to string and destructure each pair
         let [key, value] = node.map(String);
+        // if key exists in object, add value to existing array of values
+        // else, create new array that holds values
         if (adj[key]) {
             adj[key].push(value);
         } else {
             adj[key] = [value];
         }
+        // create key, value pair in object for prereq class if doesnt exitst
         if (!adj[value]) {
             adj[value] = [];
         }
