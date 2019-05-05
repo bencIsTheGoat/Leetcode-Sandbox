a2c8de9666e513f3b8189070befc58eb2483e287
Ben Cutler <bcutler94@gmail.com>
2019-05-05 09:40:44 -0400

    Comment letterCombos

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -1,6 +1,14 @@
-var longestPalindrome = function (s) {
+// Find the longest palindrome in a string, this answer does it in quadratic time
+// complexity rather than cubic, use Manachers algo to get in linear time complex
+
+const longestPalindrome = function (s) {
+    // edge case coverage
     if (s.length <= 1) return s;
+    // initialize longest variable to keep track of longest
     let longest = '';
+    // iterate thru string calling helper function, expand, on current index of letter
+    // and the current index and index + 1 pair to check all possible palindromes
+    // of even and odd length
     for (let i = 0; i < s.length; i++) {
         let palindrome1 = expand(s, i, i);
         if (palindrome1.length > longest.length) {
@@ -16,6 +24,10 @@ var longestPalindrome = function (s) {
 };
 
 function expand(str, start, end) {
+    // takes in the string with starting and end index
+    // since the corresponding letter mirrored around center letter (median) in
+    // palindromes have to be equal, this function finds longest possible given
+    // a current index
     while (start >= 0 && end < str.length && str[start] == str[end]) {
         start--;
         end++;
