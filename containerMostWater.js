8273c131b1a39be11c616368b4fce64cb1f42bba
Ben Cutler <bcutler94@gmail.com>
2019-05-06 20:06:32 -0400

    Comment containerMostWater

--- containerMostWater.js
+++ containerMostWater.js
@@ -21,12 +21,21 @@ var maxArea = function (height) {
         // get the heigh of the rectangle which can only be as high as the smaller
         // length otherwise water will spill out of rectangle
         const minHeight = Math.min(height[start], height[end]);
+        // calculate the basic rectangle base * height formula
         const area = minHeight * width;
+        // if the running area is greater than the most recent max (total) then
+        // replace the total with new max
         if (area > total) total = area;
+        // if the left side of rectangle is smaller than the right side, increment
+        // inside the array towards the right to try next rectangle with larger
+        // right side
         if (height[start] < height[end]) {
             start++;
+        // same thing but opposite
         } else if (height[start] > height[end]) {
             end--;
+        // if the heights are equal we already have that max height so lets move
+        // both pointers in to keep others
         } else {
             start++;
             end--;
