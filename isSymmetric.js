502dc9f31a19029ebfed6f28f231f50ce45733f2
Ben Cutler <bcutler94@gmail.com>
2019-05-09 17:41:48 -0400

    Add isSymm

---
+++ isSymmetric.js
@@ -0,0 +1,17 @@
+
+
+var isSymmetric = function (root) {
+    if (!root) return true;
+    if (!root.right && !root.left) return true;
+    if (!root.right || !root.left) return false;
+    return recurHelper(root.left, root.right);
+};
+
+function recurHelper(left, right) {
+    if (!left && !right) return true;
+    if (!right || !left) return false;
+    const innerCheck = left.val === right.val;
+    const recurCheck1 = recurHelper(left.right, right.left);
+    const recurCheck2 = recurHelper(left.left, right.right);
+    return innerCheck && recurCheck1 && recurCheck2;
+}
