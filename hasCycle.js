7013cdaeaa2a441787e3556005529586d4bb882b
Ben Cutler <bcutler94@gmail.com>
2019-05-08 21:07:44 -0400

    Comment hasCycle

--- hasCycle.js
+++ hasCycle.js
@@ -1,8 +1,15 @@
 // Given a linked list, determine if it has a cycle in it.
 
+// use two pointer method
+
 var hasCycle = function (head) {
+    // use two pointer method that has one iterating one node at a time
     let oneSpeed = head;
+    // has the other going down the ll two nodes at a time #lightspeedbitch
     let twoSpeed = head;
+    // prolly dont need to check that both nodes are valid but too scared to change
+    // honestly worried about the twospeed skipping past the null, but pretty irrational thought
+    // especially since i have another check on line 14
     while (oneSpeed && twoSpeed) {
         if (!twoSpeed.next || !twoSpeed.next.next) return false;
         twoSpeed = twoSpeed.next.next;
