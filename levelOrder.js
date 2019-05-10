3d6a8ee2d5d872c21b407baf36ea7666906f680f
Ben Cutler <bcutler94@gmail.com>
2019-05-10 10:23:23 -0400

    Add levelOrder

--- levelOrder.js
+++ levelOrder.js
@@ -3,9 +3,14 @@ Given a binary tree, return the level order traversal of its nodes'
 values. (ie, from left to right, level by level).
 */
 
-var levelOrder = function (root, level = 0) {
+// done recursively
+var levelOrder = function (root) {
+    // initialize 2D array that will hold levels
     let levels = [];
+    // if there is no tree passed in just return empty array
     if (!root) return levels;
+    // define recursive helper function, easier in my mind to do this way since
+    // were continually interacting with levels
     const helper = (root, level = 0) => {
         if (levels.length === level) {
             levels.push([]);
