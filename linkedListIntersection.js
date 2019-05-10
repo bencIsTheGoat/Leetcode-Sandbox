5e22b8fcdc92fe6b23add32c2507c6b38b96b309
Ben Cutler <bcutler94@gmail.com>
2019-05-10 10:50:11 -0400

    Comment llintersect

--- linkedListIntersection.js
+++ linkedListIntersection.js
@@ -3,13 +3,16 @@
 
 // used set to store first LL and then check second LL in set
 var getIntersectionNode = function (headA, headB) {
+    // init set to store the ndoes of first ll
     const set = new Set();
     let current1 = headA;
+    // iterate thru first ll and add all nodes to set
     while (current1) {
         set.add(current1);
         current1 = current1.next;
     }
     let current2 = headB;
+    // iterate thru the second ll and check if any of the nodes are in the set
     while (current2) {
         if (set.has(current2)) return current2;
         current2 = current2.next;
