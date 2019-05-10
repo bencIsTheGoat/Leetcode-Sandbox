2d30818939abac134116171ee313a5d5c768224f
Ben Cutler <bcutler94@gmail.com>
2019-05-10 11:23:23 -0400

    Comment lrucache

--- lruCache.js
+++ lruCache.js
@@ -25,23 +25,37 @@ var LRUCache = function (capacity) {
 // high level: grab the element with the given key from the obj and use that to
 // remove from double linked list and add to the front since its most recently used
 LRUCache.prototype.get = function (key) {
+    // find element in object
     const ele = this.obj[key];
+    // if theres no element return -1
     if (!ele) return -1;
+    // remove the element from the dll
     ele.remove();
+    // overwrite the element in the object with dll element that has been added
+    // to end of dll
     this.obj[key] = this.dll.append(ele.key, ele.val);
+    // return the value
     return ele.val;
 };
 
-
+// basically does the same thing as get but also prunes the cache if size is greater
+// than the capacity
 LRUCache.prototype.put = function (key, value) {
+    // find the node in the object
     const ele = this.obj[key];
+    // if the element exists remove it from the dll
     if (ele) {
         ele.remove()
+        // decrease length of lrucache
         this.size--;
     }
+    // add the node to the end of dll (cause most recent) and overwrite old value
     this.obj[key] = this.dll.append(key, value);
+    // increase legnth of lrucache
     this.size++;
+    // if the size of the lru cache is greater than the capacity prune the lrucache
     if (this.size > this.capacity) {
+        // call dll remove first method that returns the node to be removed
         const rNode = this.dll.removeFirst();
         delete this.obj[rNode.key];
         this.size--;
