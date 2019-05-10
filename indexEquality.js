e379de476c2acb5e54778a3e25f5645815367e64
Ben Cutler <bcutler94@gmail.com>
2019-05-10 10:35:12 -0400

    Comment indexEqual

--- indexEquality.js
+++ indexEquality.js
@@ -3,46 +3,30 @@
 // Given a sorted array of numbers, return the lowest index at which the index 
 // is equal to the value at that index, otherwise return -1
 
-// function indexEquality(array) {
-//     // grab the middle index of the sorted array, indicates the 'root' node
-//     // of binary tree
-//     let middleIdx = Math.floor(array.length / 2);
-//     let current = array[middleIdx];
-//     // find the height of tree for max number of iterations
-//     let log = Math.ceil(Math.log2(array.length + 1)) - 1;
-//     // keep track of current height of tree for the middle index of the right
-//     // side of the sorted array, want to make sure we are finding the middle index of
-//     // increasingly shorter range
-//     let rightCount = 0
-//     while (log >= 0) {
-//         rightCount += 1;
-//         log -= 1
-//         // if the index and element equal return the element
-//         if (array[middleIdx] === middleIdx) {
-//             return array[middleIdx];
-//         } else if (middleIdx < current) {
-//             // if the index is less than the middle element we know we have to 
-//             // go down the left side of the tree
-//             middleIdx = Math.floor(middleIdx / 2);
-//         } else {
-//             // otherwise we go down the right side of the tree
-//             middleIdx = middleIdx + Math.round(middleIdx / (2 * rightCount));
-//         } 
-//     }
-//     return -1;
-// }
+// can do easily in linear time but can do better in log time
 
 function indexEquality(array) {
+    // create two pointers, start and end
     let start = 0;
     let end = array.length;
+    // keep track of ans because we want the leftmost element that matches with its
+    // index
     let ans = -1;
+    // perform operations while the start pointer is less than the end pointer
     while (start < end) {
+        // calculate the average index, need this as we change pointers and need to
+        // grab the middle index
         const midIdx = Math.floor((start + end) / 2);
+        // if the index and element match each other set the ans to the index, then
+        // set the end pointer to middle index because we want to keep checking left
         if (array[midIdx] === midIdx) {
             ans = midIdx;
             end = midIdx;
+        // if the array element is less than the index we know we must go right, so
+        // set the start pointer to the middle
         } else if (array[midIdx] < midIdx) {
             start = midIdx + 1;
+        // if the array element is greater than the index we must go left
         } else {
             end = midIdx;
         }
