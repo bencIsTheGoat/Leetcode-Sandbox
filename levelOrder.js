b07fa2b21e641c30225c72c35255cce7284d493b
Ben Cutler <bcutler94@gmail.com>
2019-05-10 10:26:14 -0400

    Add levelOrder

--- levelOrder.js
+++ levelOrder.js
@@ -12,10 +12,13 @@ var levelOrder = function (root) {
     // define recursive helper function, easier in my mind to do this way since
     // were continually interacting with levels
     const helper = (root, level = 0) => {
+        // add an array for each level into the levels array
         if (levels.length === level) {
             levels.push([]);
         }
+        // key into levels in order to push the root value into its appropriate level
         levels[level].push(root.val);
+        // call the function on the level and right node if they exist
         if (root.left) {
             helper(root.left, level + 1);
         }
@@ -23,6 +26,7 @@ var levelOrder = function (root) {
             helper(root.right, level + 1)
         }
     }
+    // call the function here to run the recursion helper
     helper(root, 0);
     return levels;
 }
