4ec99fad5076ce6a8ae4a878af359216535dd57a
Ben Cutler <bcutler94@gmail.com>
2019-05-16 10:51:38 -0400

    Comment zigzagTraversal

--- heapClass.js
+++ heapClass.js
@@ -19,32 +19,44 @@ class MaxHeap {
         if (this.array.length <= 1) return null;
         // grab the old max to return later, gangster
         let max = this.array[1];
+        // remove the last element and set the first to it
         this.array[1] = this.array.pop()
+        // call siftdown method
         this.siftDown(1);
         return max;
     }
 
     siftUp(idx) {
+        // base case: if the index is 1 we know were at the max
         if (idx === 1) return;
+        // get the parents index
         const parentIdx = this.getParent(idx);
+        // if the parent is less than the child then perform switch
         if (this.array[parentIdx] < this.array[idx]) {
             [this.array[parentIdx], this.array[idx]] = [this.array[idx], this.array[parentIdx]];
+            // call recursively on same but sifted up element
             this.siftUp(parentIdx);
         }
     }
 
     siftDown(idx) {
+        // get the left childs index
         const left = this.getLeftChild(idx);
+        // get the right childs index
         const right = this.getRightChild(idx);
+        // get the val of the left child if it exists
         const leftVal = this.array[left] ? this.array[left] : -Infinity;
+        // get the val of the right child if it exists
         const rightVal = this.array[right] ? this.array[right] : -Infinity;
+        // if the current index is greater than both of its children we know its good
         if (this.array[idx] > Math.max(leftVal, rightVal)) return;
-        let swapIdx = 0;
+        // find the index of the child that has greater value and save to swapIdx
         if (rightVal > leftVal) {
-            swapIdx = right;
+            var swapIdx = right;
         } else {
-            swapIdx = left;
+            var swapIdx = left;
         }
+        // 
         [this.array[idx], this.array[swapIdx]] = [this.array[swapIdx], this.array[idx]];
         this.siftDown(swapIdx);
     }
--- zigzagTraversal.js
+++ zigzagTraversal.js
@@ -4,11 +4,21 @@ values. (ie, from left to right, then right to left for the next level and
 alternate between).
 */
 
+// similar to level traversal function but we do shift and unshift depending on
+// whether index is odd or even
 var zigzagLevelOrder = function (root) {
+    // initialize empty levels array that will turn into 2D array with levels
     let levels = [];
+    // edge case: if no tree then return empty array
     if (!root) return levels;
+    // recursion helper function in which we pass in level and array
     const recurHelper = (root, level) => {
+        // base case: if the root doesnt exist just return, meaning we've reached the
+        // bottom of the current tree route/root??
         if (!root) return;
+        // adding new empty subarray for each level
+        // if the length of the levels array is equal to the level (arg passed in)
+        // then push new empty subarray in
         if (levels.length === level) levels.push([]);
         const evenBool = level % 2 === 0;
         if (evenBool) {
