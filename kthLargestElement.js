c5fb085944cbfe2b61b25a1b1996d0347d17ccac
Ben Cutler <bcutler94@gmail.com>
2019-05-16 10:12:40 -0400

    Comment kthLargest

--- kthLargestElement.js
+++ kthLargestElement.js
@@ -20,33 +20,53 @@ var findKthLargest = function (nums, k) {
 };
 
 function insert(heap, num) {
+    // push the num into the heap
     heap.push(num);
+    // create recursive function that is the classic siftUp method used for heaps
     const siftUp = (idx) => {
+        // grab the current indexes parent index
         const parentIdx = Math.floor(idx / 2);
+        // base case: if the index is at 1 we know its the max
         if (idx === 1) return;
+        // if the current index is greater than its parent then switch the two in array
         if (heap[idx] > heap[parentIdx]) {
             [heap[parentIdx], heap[idx]] = [heap[idx], heap[parentIdx]];
+            // call the recursive function in on its parentIdx, to check if same
+            // element needs to sifted up again
             siftUp(parentIdx);
         }
     }
+    // call the recursive helper method on current heap to sift up most recent number
     siftUp(heap.length - 1);
 }
 
 function removeMax(heap) {
+    // remove the last index of the heap and make it the max
     heap[1] = heap.pop();
+    // recursive helper function that is class siftDown method
     const siftDown = (idx) => {
+        // find the left childs index
         const leftIdx = idx * 2;
+        // find the right childs index
         const rightIdx = idx * 2 + 1;
+        // grab the left child if it exists
         const left = heap[leftIdx] ? heap[leftIdx] : -Infinity;
+        // grab the right child if it exists
         const right = heap[rightIdx] ? heap[rightIdx] : -Infinity;
+        // base case: if the current index is greater than both of the children then okay
         if (heap[idx] > Math.max(left, right)) return;
+        // check here is to swtich the current index with the greater of the two children
+        // and then set the swapIdx to the greater childs index
         if (left > right) {
             var swapIdx = leftIdx;
         } else {
             var swapIdx = rightIdx;
         }
+        // perform sifting down operations 
         [heap[idx], heap[swapIdx]] = [heap[swapIdx], heap[idx]];
+        // call recursively on the same element that is sifted down
         siftDown(swapIdx);
     }
+    // call function on current heap passing in index 1 for max
     siftDown(1);
 }
