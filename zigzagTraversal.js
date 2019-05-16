e2fc779556ccbf994741fcbc203233a1ab8ed794
Ben Cutler <bcutler94@gmail.com>
2019-05-16 10:56:30 -0400

    Comment zigzagTraversal

--- zigzagTraversal.js
+++ zigzagTraversal.js
@@ -20,15 +20,25 @@ var zigzagLevelOrder = function (root) {
         // if the length of the levels array is equal to the level (arg passed in)
         // then push new empty subarray in
         if (levels.length === level) levels.push([]);
+        // save a bool to variable checking whether then level is even or not
         const evenBool = level % 2 === 0;
+        // if the level is even we want to push element into array indicating
+        // a level traversal from left to right
         if (evenBool) {
+            // push element into correct subarray using level from arg
             levels[level].push(root.val);
+        // if the level is odd we want to unshift the element, indicating a level
+        // traversal from right to left
         } else {
+            // unshift element into correct subarray using level from arg
             levels[level].unshift(root.val);
         }
+        // call the recursive function on the left and right child while increment
+        // the level by 1
         recurHelper(root.left, level + 1);
         recurHelper(root.right, level + 1);
     }
+    // call the recursive helper function on the root node passing it zero for level
     recurHelper(root, 0);
     return levels;
 };
