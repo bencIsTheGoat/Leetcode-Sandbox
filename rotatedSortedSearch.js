343e0ce89a56763b99503fd29b7013b09e3af53f
Ben Cutler <bcutler94@gmail.com>
2019-05-19 16:17:46 -0400

    Finish rotate sortedSearch

--- rotatedSortedSearch.js
+++ rotatedSortedSearch.js
@@ -1,11 +1,10 @@
-//  [10, -5, -4, 3, -1]
+// Find target in sorted array that could be rotated
 
 // find the divide or max in array and index
 // arr[middle] > arr[end] && range > maxRange
 // if 2ndtolast is less than end then decrement end by 1
 // basically treat the searches at 2 different arrays
 
-// [0, 1, 5, -4]
 function rotatedSearch (array, targ) {
     const partIdx = idxHelper(array);
     if (targ < array[0]) {
