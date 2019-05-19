41a3eda196aed82898af9e558bc05d172e8ab0dd
Ben Cutler <bcutler94@gmail.com>
2019-05-18 20:05:09 -0500

    Add movingAverage

--- movingAverage.js
+++ movingAverage.js
@@ -1,19 +1,14 @@
 // Given a stream of integers and a window size, calculate the moving average 
 // of all integers in the sliding window.
 
-
-
 var MovingAverage = function (size) {
     this.obj = {};
     this.dll = new DoubleLinkedList ();
     this.size = size;
+    this.currentSize = 0;
 };
 
 MovingAverage.prototype.next = function (val) {
-    this.array.push(val);
-    if (this.array.length > this.size) this.array.shift();
-    const sum = this.array.reduce((acc, num) => acc + num);
-    return sum / this.array.length;
 };
 
 class ListNode {
