54646a9c4d6bb032c77d26119f82b013a7e3bf5e
Ben Cutler <bcutler94@gmail.com>
2019-05-19 08:47:33 -0500

    Add first method rotated

---
+++ rotatedSortedSearch.js
@@ -0,0 +1,25 @@
+//  [10, -5, -4, 3, -1]
+
+// find the divide or max in array and index
+// arr[middle] > arr[end] && range > maxRange
+// if 2ndtolast is less than end then decrement end by 1
+// basically treat the searches at 2 different arrays
+
+// [0, 1, 5, -4]
+function rotatedSearch (array) {
+    let start = 0;
+    let end = array.length - 1;
+    while (start < end - 1) {
+        const middle = Math.floor((start + end) / 2);
+        if (array[middle] > array[end]) {
+            start = middle;
+            if (array[end - 1] <= array[end]) end--;
+        } else {
+            end = middle;
+        }
+        console.log([start, end])
+    }
+    return start;
+}
+
+console.log(rotatedSearch([0, 1, 2, -39, -25, -17, -10, -5, -2]))
