b4ac610f28eea2b34ba1c14f2ac8ccea810bd3c8
Ben Cutler <bcutler94@gmail.com>
2019-05-20 15:45:20 -0400

    Add diagonalMatrix

--- diagonalMatrix.js
+++ diagonalMatrix.js
@@ -1,4 +1,5 @@
-
+// Given a matrix of M x N elements (M rows, N columns), return all elements 
+// of the matrix in diagonal order as shown in the below image.
 
 var findDiagonalOrder = function (matrix) {
     let output = [];
