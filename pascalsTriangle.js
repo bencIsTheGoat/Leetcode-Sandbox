0552871ca9e4212ca9fee8fe964a45c2d9042898
Ben Cutler <bcutler94@gmail.com>
2019-05-20 17:19:33 -0400

    Add pascalsTriangle

---
+++ pascalsTriangle.js
@@ -0,0 +1,22 @@
+var generate = function (numRows) {
+    let triangle = [];
+    let count = 1;
+    while (count <= numRows) {
+        if (count === 1) {
+            triangle.push([1]);
+        } else if (count === 2) {
+            triangle.push([1, 1]);
+        } else {
+            let level = [1];
+            const lastLevel = triangle[count - 2];
+            for (let i = 0; i < lastLevel.length - 1; i++) {
+                const newNum = lastLevel[i] + lastLevel[i + 1];
+                level.push(newNum);
+            }
+            level.push(1);
+            triangle.push(level);
+        }
+        count++;
+    }
+    return triangle;
+};
