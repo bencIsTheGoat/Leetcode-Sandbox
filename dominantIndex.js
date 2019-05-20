d4cd64cf745bd3113b61fa995e980507e56cd8fe
Ben Cutler <bcutler94@gmail.com>
2019-05-20 11:12:59 -0400

    Add dominantIndex

--- dominantIndex.js
+++ dominantIndex.js
@@ -1,4 +1,11 @@
+/*
+In a given integer array nums, there is always exactly one largest element.
 
+Find whether the largest element in the array is at least twice as much as 
+every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.
+*/
 
 var dominantIndex = function (nums) {
     let max = nums[0];
