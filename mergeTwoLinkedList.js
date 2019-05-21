c84a0507ab0d1ac5e93f41cdff860f690e6acdb5
Ben Cutler <bcutler94@gmail.com>
2019-05-21 08:13:07 -0400

    Add mergeTwoLinkedList

---
+++ mergeTwoLinkedList.js
@@ -0,0 +1,22 @@
+// Merge two sorted linked lists and return it as a new list. The new list 
+// should be made by splicing together the nodes of the first two lists.
+
+var mergeTwoLists = function (l1, l2) {
+    if (l1 === null && l2 === null) {
+        return null;
+    } else if (l1 === null) {
+        return l2;
+    } else if (l2 === null) {
+        return l1
+    }
+    const val1 = l1 ? l1.val : -Infinity;
+    const val2 = l2 ? l2.val : -Infinity;
+    if (val1 < val2) {
+        var node = new ListNode(val1);
+        node.next = mergeTwoLists(l1.next, l2);
+    } else {
+        var node = new ListNode(val2);
+        node.next = mergeTwoLists(l1, l2.next);
+    }
+    return node;
+};
