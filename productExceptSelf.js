ed2ac571d388985b27626d8fdee6f409dd04a9bc
Ben Cutler <bcutler94@gmail.com>
2019-05-21 14:06:45 -0400

    Add productExceptSelf

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -1,4 +1,7 @@
-
+/*
+Given an array nums of n integers where n > 1,  return an array output such that
+output[i] is equal to the product of all the elements of nums except nums[i].
+*/
 
 var productExceptSelf = function (nums) {
     if (nums.length <= 1) return [];
