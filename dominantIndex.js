18c0789a9b19fb552a8d66a5cec0d1e029f71d1a
Ben Cutler <bcutler94@gmail.com>
2019-05-24 07:22:53 -0400

    Comment dominant index

--- dominantIndex.js
+++ dominantIndex.js
@@ -9,14 +9,22 @@ If it is, return the index of the largest element, otherwise return -1.
 
 // one iteration
 var dominantIndex = function (nums) {
+    // set max to first ele
     let max = nums[0];
+    // keep track of max index
     let maxIdx = 0;
+    // keep track of second largest element's index
     let secondMax = -Infinity;
     for (let i = 1; i < nums.length; i++) {
+        // if the current element is greater than max, set the current max to 
+        // second largest max, set the current element to new largest max, and
+        // max index
         if (nums[i] > max) {
             secondMax = max;
             max = nums[i];
             maxIdx = i;
+        // if the current index is smaller than or equal to max but greater than
+        //  current second max, reassign the second max to the currrent elememnt
         } else if (nums[i] > secondMax) {
             secondMax = nums[i];
         }
