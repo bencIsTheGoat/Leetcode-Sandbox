81a60cfa694548322d908637e2895536b2c63477
Ben Cutler <bcutler94@gmail.com>
2019-05-24 11:25:10 -0400

    Comment movingAverage

--- movingAverage.js
+++ movingAverage.js
@@ -3,33 +3,49 @@
 
 // Utilize LRU Cache methodology
 var MovingAverage = function (size) {
+    // define obj for constant lookup, keep track of integers
     this.obj = {};
+    // define dll
     this.dll = new DoubleLinkedList ();
+    // define maximum size for window
     this.size = size;
+    // keep track of the current size
     this.currentSize = 0;
+    // keep track of the sum of integers
     this.sum = 0;
 };
 
 MovingAverage.prototype.next = function (val) {
+    // append the new value into the dll
     this.dll.append(val);
+    // increment the sum by the val
     this.sum += val;
+    // increment the current size by the size
     this.currentSize++;
+    // if the current size is greater than max size then we must prune
     if (this.currentSize > this.size) {
+        // remove the first node of the dll
         const delNode = this.dll.removeFirst();
+        // delete the node val from total sum
         this.sum -= delNode.val;
+        // decrement the current size after removal
         this.currentSize--;
     }
+    // return the movingaverage
     return this.sum / this.currentSize
 };
 
 class ListNode {
     constructor(val) {
+        // has to have prev and next for dll
         this.next = null;
         this.prev = null;
         this.val = val;
     }
     remove() {
+        // set the prev next to next
         this.prev.next = this.next;
+        // set the next prev to prev
         this.next.prev = this.prev;
         this.next = null;
         this.prev = null;
@@ -65,8 +81,11 @@ class DoubleLinkedList {
     }
 
     removeFirst () {
+        // grab the first node in the list after head
         const node = this.head.next;
+        // call node remove method on it
         node.remove();
+        // return the removed node for later use
         return node;
     }
 }
