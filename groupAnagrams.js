3294c7526a6bc75516d2456f0f7b0b1dc610c71a
Ben Cutler <bcutler94@gmail.com>
2019-05-24 07:30:40 -0400

    Comment group anagrams

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -1,5 +1,9 @@
+// Group the anagrams of an array of strings
+
 var groupAnagrams = function (strs) {
+    // initialize hashmap of letters and their indexes
     const alpha = { 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25 };
+    
     const obj = {};
     for (let word of strs) {
         console.log(word)
