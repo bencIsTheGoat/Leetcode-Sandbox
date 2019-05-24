ca5691237bd6307f839bed35bb1a590539e02a1e
Ben Cutler <bcutler94@gmail.com>
2019-05-24 11:37:58 -0400

    Comment palindromeNum

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -2,9 +2,14 @@
 // it reads the same backward as forward.
 
 var isPalindrome = function (x) {
+    // any negative number is false
     if (x < 0) return false;
+    // save num for later comparison
     let val = x;
+    // create variable for palindrome number
     let palNum = 0;
+    // while the val is greater than zero, grab the tens digit and add it to pal
+    // number while multiplying old pal number by 10 to move it over 1
     while (val > 0) {
         const mod = val % 10;
         palNum = (palNum * 10) + mod;
