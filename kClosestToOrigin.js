ea1cc383fb0fed2e055c76fb2548e2cc985aa93d
Ben Cutler <bcutler94@gmail.com>
2019-05-24 08:38:41 -0400

    Comment k closest

--- kClosestToOrigin.js
+++ kClosestToOrigin.js
@@ -10,12 +10,16 @@ You may return the answer in any order.  The answer is guaranteed to be unique
 var kClosest = function (points, K) {
     // init array
     const array = [];
+    // edge case coverage
     if (points.length <= K) return points;
+    // iterate thru points and find the distance
     for (let coord of points) {
         const [x, y] = coord;
         const dist = Math.sqrt((x ** 2) + (y ** 2));
+        // store coord and dist information in array
         array.push({ dist: dist, coord: coord });
     }
+
     array.sort((a, b) => a.dist - b.dist);
     let output = array.map((obj, idx) => {
         return obj['coord'];
