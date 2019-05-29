b7213b02f562a9001433cb3a6d11bd83c3a64ee9
Ben Cutler <bcutler94@gmail.com>
2019-05-29 10:27:23 -0400

    Add maxDepth

--- maxDepth.js
+++ maxDepth.js
@@ -1,3 +1,8 @@
+// Given a binary tree, find its maximum depth.
+
+// The maximum depth is the number of nodes along the longest path from the 
+// root node down to the farthest leaf node.
+
 var maxDepth = function (root) {
     if (root === null) return 0;
     const left = maxDepth(root.left);
