5016417578879ef9bf1b76e46471ceae6820d44a
Ben Cutler <bcutler94@gmail.com>
2019-05-29 08:02:40 -0400

    Add climbStairs

--- climbStairs.js
+++ climbStairs.js
@@ -1,3 +1,7 @@
+// You are climbing a stair case.It takes n steps to reach to the top.
+
+// Each time you can either climb 1 or 2 steps.In how many distinct ways can you climb to the top ?
+
 function climbStairs(n, memo = {}) {
     if (n in memo) return memo[n];
     if (n === 0) return 0;
