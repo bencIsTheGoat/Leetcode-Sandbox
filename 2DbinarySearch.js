14cf4e97e5bd0b28e226f99d7dd0d5024cd5f987
Ben Cutler <bcutler94@gmail.com>
2019-07-01 12:26:31 -0400

    Add searchMatrix

--- 2DbinarySearch.js
+++ 2DbinarySearch.js
@@ -1,3 +1,22 @@
+function searchMatrix(matrix, target) {
+    if (matrix.length === 0) return false;
+    let x = 0;
+    let y = matrix.length - 1;
+    while (x < matrix[0].length && y >= 0) {
+        console.log([y, x])
+        if (target === matrix[y][x]) return true;
+        const right = matrix[y][x + 1] ? matrix[y][x + 1] : Infinity;
+        if (target > right) {
+            x++;
+        } else if (target < right) {
+            y--;
+        } else if (target === right) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function searchMatrix(matrix, target, x1 = 0, x2 = matrix[0] ? matrix[0].length : 0, y1 = 0, y2 = matrix.length) {
     let x = Math.floor((x1 + x2) / 2);
     let y = Math.floor((y1 + y2) / 2);
