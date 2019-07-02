3f99c9a5e51b9b76181ebd90dca37ce9eef65c4b
Ben Cutler <bcutler94@gmail.com>
2019-07-02 08:08:36 -0400

    nQueens

--- nQueens.js
+++ nQueens.js
@@ -1,46 +1,57 @@
-var totalNQueens = function (n, row = 0, count = 0, rowSet = new Set(), colSet = new Set(), diagArr = new Set()) {
+var solveNQueens = function (n, row = 0, rowSet = new Set(), colSet = new Set(), diagSet = new Set(), output = queenBoard(n), res = []) {
     let i = 0;
     while (i < n) {
-        if (safe(row, i, rowSet, colSet, diagArr)) {
-            place(row, i, rowSet, colSet, diagArr);
+        if (safe(row, i, rowSet, colSet, diagSet)) {
+            place(row, i, rowSet, colSet, diagSet, output);
             if (row + 1 === n) {
-                count++;
+                res.push(stringBoard(output));
             } else {
-                count = totalNQueens(n, row + 1, count, rowSet, colSet, diagArr);
+                res = solveNQueens(n, row + 1, rowSet, colSet, diagSet, output, res);
             }
-            remove(row, i, rowSet, colSet, diagArr)
+            remove(row, i, rowSet, colSet, diagSet, output, res);
         }
+        output[row][i] = '.'
         i++;
     }
-    return count;
+    return res;
 };
 
-function remove(row, col, rowSet, colSet, diagArr) {
-    rowSet.delete(row);
-    colSet.delete(col);
-    for (let pair of diagArr) {
-        const arr = pair.split(',');
-        const y = parseInt(arr[0]);
-        const x = parseInt(arr[1]);
-        if (row === y && col === x) diagArr.delete(pair);
-    }
+function stringBoard(board) {
+    return board.map(ele => ele.join(''));
 }
 
-function place(row, col, rowSet, colSet, diagArr) {
-    rowSet.add(row);
-    colSet.add(col);
-    diagArr.add(String([row, col]));
+function queenBoard(n) {
+    let board = [...new Array(n)].map(ele => new Array(n).fill('.'));
+    return board
 }
 
-function safe(row, col, rowSet, colSet, diagArr) {
+function safe(row, col, rowSet, colSet, diagSet) {
     const rowCheck = !rowSet.has(row);
     const colCheck = !colSet.has(col);
-    let diagCheck = true;
-    for (let pair of diagArr) {
+    for (const pair of diagSet) {
         const arr = pair.split(',');
         const y = parseInt(arr[0]);
         const x = parseInt(arr[1]);
-        if (Math.abs(row - y) === Math.abs(col - x)) diagCheck = false;
+        if (Math.abs(row - y) === Math.abs(col - x)) return false;
     }
-    return rowCheck && colCheck && diagCheck;
-}
+    return rowCheck && colCheck;
+};
+
+function place(row, col, rowSet, colSet, diagSet, queenBoard) {
+    rowSet.add(row);
+    colSet.add(col);
+    diagSet.add(String([row, col]));
+    queenBoard[row][col] = "Q";
+};
+
+function remove(row, col, rowSet, colSet, diagSet, queenBoard) {
+    queenBoard[row][col] = '.';
+    rowSet.delete(row);
+    colSet.delete(col);
+    for (const pair of diagSet) {
+        const arr = pair.split(',');
+        const y = parseInt(arr[0]);
+        const x = parseInt(arr[1]);
+        if (row === y && col === x) diagSet.delete(pair);
+    }
+};
