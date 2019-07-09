c8bc8bac459f5362c1c634f13cdc6e39589eb2f7
Ben Cutler <bcutler94@gmail.com>
2019-07-09 15:55:28 -0400

    Add cloneGraph

---
+++ cloneGraph.js
@@ -0,0 +1,20 @@
+var cloneGraph = function (node) {
+
+    const queue = [node];
+    const obj = {}
+    obj[node.val] = new Node(node.val);
+    obj[node.val].neighbors = [];
+    while (queue.length > 0) {
+        const curr = queue.shift();
+        for (const child of curr.neighbors) {
+            if (!(child.val in obj)) {
+                obj[child.val] = new Node(child.val);
+                obj[child.val].neighbors = [];
+                queue.push(child)
+            }
+            obj[curr.val].neighbors.push(obj[child.val]);
+        }
+
+    }
+    return obj[node.val];
+};
