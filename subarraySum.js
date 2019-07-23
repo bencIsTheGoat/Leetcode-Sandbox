263fae4723c7069caebf911b0f0b25e458874171
Ben Cutler <bcutler94@gmail.com>
2019-07-23 10:20:13 -0400

    Add subarraysum

--- subarraySum.js
+++ subarraySum.js
@@ -1,3 +1,20 @@
+function subarraySum(nums, k) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let sum = nums[i];
+        if (sum === k) {
+            count++;
+        }
+        for (let j = i + 1; j < nums.length; j++) {
+            sum += nums[j]
+            if (sum === k) {
+                count++
+            }
+        }
+    }
+    return count;
+}
+
 var subarraySum = function (nums, k, memo = {}) {
     if (nums.length === 0) return 0;
     let count = Infinity
