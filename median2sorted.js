9912e236099b960c187375141c1115e2d5f2520e
Ben Cutler <bcutler94@gmail.com>
2019-09-25 09:15:00 -0400

    Add log time solution

--- median2sorted.js
+++ median2sorted.js
@@ -1,3 +1,35 @@
+function findMedianSortedArrays(nums1, nums2) {
+    let smallArr;
+    let largeArr;
+    let smallLeft;
+    let smallRight;
+    if (nums2.length >= nums1.length) {
+        return medianHelper(nums1, nums2)
+    } else {
+        return medianHelper(nums2, nums1)
+    }
+}
+
+function medianHelper(shortArray, longArray, smallLeft = 0, smallRight = shortArray.length) {
+    const midShort = Math.floor((smallLeft + smallRight) / 2);
+    const midLong = Math.floor((shortArray.length + longArray.length + 1) / 2) - midShort;
+    if (midShort < smallRight && shortArray[midShort] < longArray[midLong - 1]) {
+        return medianHelper(shortArray, longArray, smallLeft + 1, smallRight);
+    } else if (midShort > smallLeft && shortArray[midShort - 1] > longArray[midLong]) {
+        return medianHelper(shortArray, longArray, smallLeft, smallRight - 1);
+    } else {
+        let maxLeft = Math.max(shortArray[midShort - 1], longArray[midLong - 1]);
+        if (midShort === 0) maxLeft = longArray[midLong - 1];
+        if (midLong === 0) maxLeft = shortArray[midShort - 1];
+        if ((shortArray.length + longArray.length) % 2 === 1) return maxLeft;
+        let minRight = Math.min(shortArray[midShort], longArray[midLong]);
+        if (midShort === shortArray.length) minRight = longArray[midLong];
+        if (midLong === longArray.length) minRight = shortArray[midShort];
+
+        return (maxLeft + minRight) / 2
+    }
+}
+
 var findMedianSortedArrays = function (nums1, nums2) {
     const totalNum = nums1.length + nums2.length;
     const isEven = totalNum % 2 === 0;
